f86f482c01eb67285c7f503839c600de0f073fad
Pat3ick390s <Patrickenenche7@gmail.com>
2022-07-08 13:54:05 +0100

    Try and catch

---
+++ Expections/Expections/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.WriteLine("INPUT FIRST NUMBER");
+    string number1 = Console.ReadLine();
+
+    Console.WriteLine("INPUT SECOND NUMBER");
+    string number2 = Console.ReadLine();
+
+    Console.WriteLine("FINALY ANSWER..!");
+    Console.WriteLine((decimal)int.Parse(number1) / int.Parse(number2));
+}
+catch (FormatException)
+{
+    Console.WriteLine("SORRY, STRING NOT ALLOWED !");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("SORRY, YOU CAN'T DIVIDE BY ZERO !");
+}
+catch (Exception err)
+{
+    Console.WriteLine(err);
+}
+finally
+{
+    Console.WriteLine("THANK YOU FOR ATTEMPTING !");
+}
+
---
+++ HandlingExpection/HandlingExpection/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("INPUT FIRST NUMBER");
+    string number1 = Console.ReadLine();
+
+    Console.WriteLine("INPUT SECOND NUMBER");
+    string number2 = Console.ReadLine();
+
+    Console.WriteLine("FINALY ANSWER..!");
+    Console.WriteLine((decimal)int.Parse(number1) / int.Parse(number2));
+}
+catch (FormatException)
+{
+    Console.WriteLine("SORRY, STRING NOT ALLOWED");
+}
