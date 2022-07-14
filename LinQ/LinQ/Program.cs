21ea0d5a8a68bd9283082454fc35501b7046066d
Pat3ick390s <Patrickenenche7@gmail.com>
2022-07-14 14:37:00 +0100

    LINQ

---
+++ LinQ/LinQ/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinQ
+{
+    class Progarm
+    {
+        static void Main(string[] args)
+        {
+            List<Student> Students = new List<Student>()
+            {
+                new Student("Samuel", "Akpa", "JSS1", 50.7),
+                new Student("John", "Musa", "JSS2", 70.1),
+                new Student("Peter", "Agbo", "JSS3", 49.7),
+                new Student("Simon", "Sunday", "SS1", 44.5),
+                new Student("Esther", "Omo", "SS2", 70.1),
+                new Student("Patrick", "Eche", "SS3", 95.5),
+                new Student("Abigirl", "Okpe", "SS3", 70.5),
+
+            };
+
+            ////Using the QuerySytanx.
+            //var Querysytanx = from obj in Students
+            //                  where obj.CurrentClass == "JSS1"
+            //                  select obj;
+
+
+            ////Using the Method Syntax.
+            //var methodSyntax = Students.Where(obj => obj.CurrentClass == "JSS1");
+
+
+            ////Usung the MixSytnax.
+            //var mixSytanx = (from obj in Students
+            //                 select obj.ResultAverage).Max();
+            //Console.WriteLine("Highest Average " + mixSytanx);
+
+
+            //CLASS WORK BELOW..
+            var methodSyntax = from obj in Students
+                              where obj.Fristname == "JSS2" && obj.ResultAverage >=50
+                              select obj;
+
+
+
+            foreach (var student in methodSyntax)
+            {
+                Console.WriteLine($" Fristname:{student.Fristname} Surname:{student.Surname} CurrentClass:{student.CurrentClass} ResultAverage:{student.ResultAverage}\n");
+            }
+        }
+    }
+}
---
+++ LinQ/LinQ/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ
+{
+    internal class Student
+    {
+        public string Fristname { get; set; }
+
+        public string Surname { get; set; }
+
+        public string CurrentClass { get; set; }
+
+        public double ResultAverage { get; set; }
+
+
+        public Student (string Frist, string Sur_name, string Current_Class, double Result_Average)
+        {
+            Fristname = Frist;
+            Surname = Sur_name;
+            CurrentClass = Current_Class;
+            ResultAverage = Result_Average;
+        }
+    }
+}
