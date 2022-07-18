ef666c67d2758d88b03ef842eb597ce59dc20d05
Pat3ick390s <Patrickenenche7@gmail.com>
2022-07-18 12:59:42 +0100

    Linq Assignment 18th Junly 2022

--- LinQ/LinQ/Program.cs
+++ LinQ/LinQ/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace LinQ
 {
     class Progarm
@@ -20,33 +19,43 @@ namespace LinQ
                 new Student("Esther", "Omo", "SS2", 70.1),
                 new Student("Patrick", "Eche", "SS3", 95.5),
                 new Student("Abigirl", "Okpe", "SS3", 70.5),
-
             };
+            ////SelectQuery.
+            var selectQuery = (from obj in Students
+                               select obj).ToList();
+            ////BasicQuery.
+            var basicQuery = Students.ToList();
 
-            ////Using the QuerySytanx.
+
+            ////MethodSyntax.
+            var methodSyntax = Students.SelectMany(obj => obj.Fristname).ToList();
+
+            //var methodSyntax = Students.SelectMany(obj => obj.Surname).ToList();
+            //Console.ReadLine();
+
+
+            //////Using the QuerySytanx.
             //var Querysytanx = from obj in Students
             //                  where obj.CurrentClass == "JSS1"
             //                  select obj;
 
 
-            ////Using the Method Syntax.
+            //////Using the Method Syntax.
             //var methodSyntax = Students.Where(obj => obj.CurrentClass == "JSS1");
 
 
-            ////Usung the MixSytnax.
+            //////Usung the MixSytnax.
             //var mixSytanx = (from obj in Students
             //                 select obj.ResultAverage).Max();
             //Console.WriteLine("Highest Average " + mixSytanx);
 
 
-            //CLASS WORK BELOW..
-            var methodSyntax = from obj in Students
-                              where obj.Fristname == "JSS2" && obj.ResultAverage >=50
-                              select obj;
+            /////CLASS WORK BELOW..
+            //var selectQuery = from obj in Students
+            //                   where obj.Fristname == "JSS2" && obj.ResultAverage >= 50
+            //                   select obj;
 
-
-
-            foreach (var student in methodSyntax)
+            foreach (var student in basicQuery)
             {
                 Console.WriteLine($" Fristname:{student.Fristname} Surname:{student.Surname} CurrentClass:{student.CurrentClass} ResultAverage:{student.ResultAverage}\n");
             }
---
+++ LinqAssignment/LinqAssignment/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+////QUESTION NUMBER 1;
+//-Were
+//-OfType
+
+////QUESTION NUMBER 2;
+//Types Of Projection.
+//-Selct
+//-SelectMany
+
+
+////QUESTION NUMBER 4;
+//-SORTING OPERTOR: Is used to mange data in a particular order, it may be ascending or decsending order.
+
+//THE TYPES OF OPERTOR ARE LISTED BELOW;
+
+//-OrderBy.
+//-OrderByasending.
+//-ThenBy.
+//-ThenByDesending.
+//-Reverse.
+
+namespace LinqAssignment
+{
+    class Progarm
+    {
+        static void Main(string[] args)
+        {
+            //SELECT METHOD.
+            Console.WriteLine("..................ALL THIS LIST OF ITEMS............");
+            var dataSource = new List<object>() {"Patrick", "Samuel", "John", "Kim", "Jacob", 2, 1, 4, 3, 6, 5, 8, 7, 10};
+            var methodSytnax1 = dataSource.Select(x => x).ToList();
+            foreach (var item in methodSytnax1)
+            {
+                Console.WriteLine(item);
+            }
+
+            //OF-TYPE,WHERE METHOD.
+            Console.WriteLine("..................DISPLAYING NAMES GREATER THAN 5 LATTER............");
+            var methodSytnax2 = dataSource.OfType<string>().Where(x => x.Length > 5).ToList();
+            foreach (var item in methodSytnax2)
+            {
+                Console.WriteLine(item);
+            }
+
+            //ORDER-BY ASCENDING METHOD.
+            Console.WriteLine("..................ORDER-BY- ASCENDING ORDER............");
+            var DataS = new List<int>() { 2, 1, 4, 3, 6, 5, 8, 7, 10 };
+            var MethodS = DataS.OrderBy(x => x).ToList();
+            foreach (var item in MethodS)
+            {
+                Console.WriteLine(item);
+            }
+
+            //ORDER-BY DESCENDING METHOD.
+            Console.WriteLine("..................ORDER-BY- DESCENDING ORDER............");
+            var MS = DataS.OrderByDescending(x => x).ToList();
+            foreach (var item in MS)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //  THEN-BY & ORDER-BY.
+            Console.WriteLine("..................THEN-BY AND ORDER-BY............");
+            List<Assignment> names = new List<Assignment>()
+            {
+                new Assignment(){FristName = "Patrick", LastName = "Eche"},
+                new Assignment(){FristName = "Samuel", LastName = "Opi"},
+                new Assignment(){FristName = "Daniel", LastName = "Oma"}
+            };
+            var Msytnax = names.OrderBy(x => x.LastName).ThenByDescending(x => x.FristName).ToList();
+
+            foreach (var item in names)
+            {
+                Console.WriteLine($"{item.FristName} {item.LastName}");
+            }
+
+
+            //  REVERSE.
+            Console.WriteLine("..................USING REVERSE METHOD............");
+            int[] number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var MethodQuery = number.Reverse();
+            foreach (var item in MethodQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
